6ca56bc38fa943c0b0b8c70052691bfe4f0acb66
Ajith Gannamaneni <agannama@uncc.edu>
2023-04-23 22:19:23 -0400

    Pushing iteration-4 changes

---
+++ front-end/app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const morgan = require('morgan');
+const methodOverride = require('method-override');
+const flash = require('connect-flash');
+
+const mainRoutes = require('./routes/mainRoutes');
+
+const app = express();
+
+
+let port = 3000;
+let host = 'localhost';
+app.set('view engine', 'ejs');
+
+
+app.use(flash());
+
+app.use('/socket.io', express.static(__dirname + '/node_modules/socket.io-client/dist'));
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('tiny'));
+app.use(methodOverride('_method'));
+
+app.get('/', (req, res) => {
+    res.render('index')
+});
+
+app.use('/main', mainRoutes);
+
+app.use((req, res, next) => {
+    let err = new Error('Server cannot locate ' + req.url);
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    console.log(err.stack);
+    if (!err.status) {
+        err.status = 500;
+        err.message = "Internal Server Error";
+    }
+    // console.log(err);
+
+    res.status(err.status);
+    res.render('error', { error: err });
+});
+
+app.listen(port, host, ()=>{
+    console.log('Server is running on port', port);
+})
---
+++ front-end/controllers/mainController.js
@@ -0,0 +1,98 @@
+
+const http = require('http');
+
+const FormData = require('form-data');
+const fs = require('fs');
+const path = require('path');
+
+const multer = require('multer');
+const { response } = require('express');
+
+exports.index = (req, res) => {
+    res.render('index')
+};
+
+exports.about = (req, res) => {
+    res.render('./common/about');
+};
+
+exports.contact = (req, res) => {
+    res.render('./common/contact');
+};
+
+exports.result = (req, res, next) => {
+    const dataToRender = req.body.dataToRender;
+    res.render('./result', {dataToRender});
+}
+
+exports.test = (reqs, ress, next) => {
+    // Define the form data to be sent
+    // const upload = multer({ dest: './back-end/' });
+    const storage = multer.diskStorage({
+        destination: function (req, file, cb) {
+          cb(null, './back-end'); // Directory where files will be uploaded
+        },
+        filename: function (req, file, cb) {
+          cb(null, file.originalname); // Set the filename to be the original filename
+        }
+      });
+      
+    const upload = multer({ storage: storage });
+    upload.single('file')(reqs, ress, function (err) {
+        if (err instanceof multer.MulterError) {
+            // Handle multer error
+            console.error(err);
+            // Send an appropriate response or throw an error as needed
+        } else if (err) {
+            // Handle other errors
+            console.error(err);
+            // Send an appropriate response or throw an error as needed
+        }
+
+        // File data can now be accessed from reqs.file
+        const file = reqs.file;
+        console.log(file); // Access file data as needed
+
+        // Create a readable stream from the file path
+        const fileStream = fs.createReadStream(file.path);
+        
+        const formData = new FormData();
+
+        formData.append('file', fileStream); // Append the file stream to the form data
+        formData.append('originalname', file.originalname);
+        // Define the options for the HTTP request
+        const options = {
+            hostname: 'localhost',
+            port: 5000,
+            path: '/upload',
+            method: 'POST',
+            headers: formData.getHeaders() // Set the headers from the form data
+        };
+    
+        // Send the HTTP request
+        const req = http.request(options, res => {
+            let responseData = '';
+            res.on('data', data => {
+                responseData = data.toString();
+                console.log(data.toString());
+                // io.emit('data', data.toString())
+                // $('#responseContainer').append(data.toString());
+            });
+
+            res.on('end', () => {
+                // After receiving all data, render it in the frontend view
+                const dataToRender = responseData;
+                ress.json(dataToRender) // Assuming the data received is in JSON format
+                // ress.render('./result', { dataToRender }); // Modify this line to pass the data to your view
+            });
+        });
+        req.on('error', error => {
+            console.error(error);
+        });
+        formData.pipe(req); // Pipe the form data to the request
+    });
+};
+
+
+
+
